e9b39d67546047e9be1b2aaa06d393c7caf5bf85
smorgashboard <hackintosh@HackintoshsMini.attlocal.net>
2024-12-05 20:59:43 -0500

    Day4 PT 2

--- day4.py
+++ day4.py
@@ -5,6 +5,7 @@ filename = "day4input.txt"
 word = "XMAS"
 
 times_found = 0
+times_found2 = 0
 
 DIRECTIONS = [
     (0, 1),    # Right
@@ -15,6 +16,13 @@ DIRECTIONS = [
     (1, -1),   # Down-left (diagonal)
     (-1, 1),   # Up-right (diagonal)
     (-1, -1)   # Up-left (diagonal)
+]
+
+NEW_DIRECTIONS =[
+    (1, 1),    
+    (1, -1),   
+    (-1, 1),   
+    (-1, -1)   
 ]
 
 def load_grid(filename):
@@ -35,7 +43,7 @@ def found_x(row, col):
 def found_m(row, col, d):
     global grid
     new_row = row + d[0]
-    new_col = row + d[1]
+    new_col = col + d[1]
     if new_row >= 0 and new_row  <= 139 and new_col <=139 and new_col >= 0:
         if grid[new_row, new_col] == "A":
             found_a(new_row, new_col, d)
@@ -49,8 +57,6 @@ def found_a(row, col, d):
         if grid[new_row, new_col] == "S":
             times_found += 1
 
-
-
 grid = load_grid(filename)
 
 print(grid.shape)
@@ -61,3 +67,72 @@ for row in range(grid.shape[0]):
             found_x(row, col)
 
 print(f"Total XMAS found: {times_found}")
+
+
+def part_2(row, col):
+    global grid
+    global times_found2
+    for d in NEW_DIRECTIONS:
+        bool1 = False
+        bool2 = False
+        search_row = row + d[0]
+        search_col = col + d[1]
+        if search_row >= 0 and search_row  <= 139 and search_col <=139 and search_col >= 0:
+            if grid[search_row, search_col] == "M":
+                new_row = row + (d[0] * -1)
+                new_col = col + (d[1] * -1)
+                if new_row >= 0 and new_row  <= 139 and new_col <=139 and new_col >= 0:
+                    if grid[new_row, new_col] == "S":
+                        bool1 = True
+                        bool2 = part_2_check_2(row, col, d)
+            elif grid[search_row, search_col] == "S":
+                new_row = row + (d[0] * -1)
+                new_col = col + (d[1] * -1)
+                if new_row >= 0 and new_row  <= 139 and new_col <=139 and new_col >= 0:
+                    if grid[new_row, new_col] == "M":
+                        bool1 = True
+                        bool2 = part_2_check_2(row, col, d)
+        if bool1 and bool2:
+            print(f"Found at {row}, {col} and direction {d} with grid {grid[search_row, search_col]} at {search_row} {search_col} and {grid[new_row, new_col]} at {new_row} {new_col}")
+            times_found2 += 1
+            break
+            
+def part_2_check_2(row, col, d):
+    global grid
+    if d[0] == 1 and d[1] == 1:
+        newd = (-1,1)
+    elif d[0] == 1 and d[1] == -1:
+        newd = (-1,-1)
+    elif d[0] == -1 and d[1] == 1:
+        newd = (1,1)
+    elif d[0] == -1 and d[1] == -1:
+        newd = (1,-1)
+
+    search_row = row + newd[0]
+    search_col = col + newd[1]
+    if search_row >= 0 and search_row  <= 139 and search_col <=139 and search_col >= 0:
+        if grid[search_row, search_col] == "M":
+            new_row = row + (newd[0] * -1)
+            new_col = col + (newd[1] * -1)
+            if new_row >= 0 and new_row  <= 139 and new_col <=139 and new_col >= 0:
+                if grid[new_row, new_col] == "S":
+                    print(f"Found at {row}, {col} and direction {d} with new direction of {newd} with grid {grid[search_row, search_col]} at {search_row} {search_col} and {grid[new_row, new_col]} at {new_row} {new_col}")
+                    return True
+                
+        elif grid[search_row, search_col] == "S":
+            new_row = row + (newd[0] * -1)
+            new_col = col + (newd[1] * -1)
+            if new_row >= 0 and new_row  <= 139 and new_col <=139 and new_col >= 0:
+                if grid[new_row, new_col] == "M":
+                    print(f"Found at {row}, {col} and direction {d} with new direction of {newd} with grid {grid[search_row, search_col]} at {search_row} {search_col} and {grid[new_row, new_col]} at {new_row} {new_col}")
+                    return True
+                
+    
+    return False
+
+for row in range(grid.shape[0]):
+    for col in range(grid.shape[1]):
+        if grid[row, col] == "A":
+            part_2(row, col)
+
+print(times_found2)
